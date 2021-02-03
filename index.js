f7411d9aa637a29ca00adcfb6895245840e16b48
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-02-03 14:16:27 -0500

    added functions to index.js

--- index.js
+++ index.js
@@ -6,38 +6,53 @@ const path = require('path');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // (Prompt) User questions to populate the README.md
-const promptUser = () =>
-    inquirer.prompt([{
-            type: 'input',
-            name: 'github',
-            message: 'What is your GitHub Username?',
-        },
-        {
-            type: 'input',
-            pname: 'project ',
-            message: 'What is your project title?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'How would you describe your project?',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Describe the installation process',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'What is this project used for?',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What is your license?.',
-            choices: [
-                "apache 2.0", "mit", "bsd-3", "none"
-            ]
-        },
-    ]);
+const promptUser = [{
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?',
+    },
+    {
+        type: 'input',
+        name: 'project ',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe the installation process',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is this project used for?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What is your license?.',
+        choices: [
+            "apache 2.0", "mit", "bsd-3", "none"
+        ]
+    },
+];
+// what is this function doing
+const writeToFile = (fileName, input) => {
+    return fs.writeFileSync(path.join(fileName, input))
+}
+
+// what is this function doing
+const runPrompts = () => {
+    inquirer.prompt(promptUser)
+        .then((responses) => {
+            console.log("README.md is in process");
+            writeToFile("README.md", generateMarkdown({
+                ...responses
+            }))
+        })
+}
+runPrompts();
