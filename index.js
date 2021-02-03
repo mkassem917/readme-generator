cd8720d36b46365c03e88d3d6dfb3318c35b12ea
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-02-03 13:30:35 -0500

    added utils, list and choices

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 // Node Modules
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-const writeFileAsync = util.promisify(fs.writeFile);
+const path = require('path');
+
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // (Prompt) User questions to populate the README.md
 const promptUser = () =>
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'github',
-      message: 'What is your GitHub Username?',
-    },
-    {
-      type: 'input',
-      pname: 'project ',
-      message: 'What is your project title?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'How would you describe your project?',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Describe the installation process',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What is this project used for?',
-    },
-    {
-      type: 'input',
-      name: 'license',
-      message: 'What is your license?.',
-    },
-  ]);
+    inquirer.prompt([{
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub Username?',
+        },
+        {
+            type: 'input',
+            pname: 'project ',
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'How would you describe your project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Describe the installation process',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is this project used for?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What is your license?.',
+            choices: [
+                "apache 2.0", "mit", "bsd-3", "none"
+            ]
+        },
+    ]);
